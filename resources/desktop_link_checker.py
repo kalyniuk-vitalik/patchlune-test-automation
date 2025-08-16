67ce70ef594a60b7d5008f7e8777a899564253e7
Vitalik Kalyniuk <vitalik.kalyniuk@avanquest.com>
2025-08-16 03:41:06 +0200

    Track resources directory

---
+++ resources/desktop_link_checker.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+def verify_desktop_to_web_flow(redirect, custom_value, lang_of_url):
+    path = Path(r"C:\ProgramData\Avanquest\Driver Updater\logs")
+    if not path.exists():
+        raise FileNotFoundError(f"Log directory not found: {path}")
+
+    installer = list(path.rglob("*installer*"))
+    if not installer:
+        raise FileNotFoundError("Not found installer log")
+
+    log_file = max(installer, key=lambda p: p.stat().st_mtime)
+    # opened_log_file = log_file.read_text(encoding="utf-8", errors="ignore")
+    try:
+        with open(log_file, encoding="utf-8", errors="ignore") as f:
+            for i, line in enumerate(f, 1):
+                if f"redirect-to. name={redirect}" in line:
+                    for j, line2 in enumerate(f, start=i+1):
+                        if f"execute. what=\"https://paygw.adaware.com/redirect/custom/pch-driver-updater?customValue={custom_value}&lang={lang_of_url}\"" in line2:
+                            return True
+            return False
+    except (OSError, IOError) as e:
+        raise RuntimeError(f"Cannot read log file")
