7be33c087a92715865d3c380b538bdefdae1bdcd
kalyniuk <113550416+kalyniuk-vitalik@users.noreply.github.com>
2026-02-05 19:45:13 +0100

    Update INSTALLING_LABEL title in locators.py

--- page_objects/installer/locators.py
+++ page_objects/installer/locators.py
@@ -4,5 +4,5 @@ class InstallerLocators:
     PRIVACY_POLICY_LINK = {"title": "privacy policy", "control_type": "Hyperlink"}
     LICENSING_AGREEMENT_LINK = {"title": "license agreement", "control_type": "Hyperlink"}
     LANGUAGE_DROPDOWN = {"control_type": "ComboBox", "found_index": 0}
-    INSTALLING_LABEL = {"title": "Installing Driver Updater", "control_type": "Text"}
+    INSTALLING_LABEL = {"title": "Installing", "control_type": "Text"}
     DO_NOT_TURN_OFF_LABEL = {"title": "Please do not turn your device off", "control_type": "Text"}
